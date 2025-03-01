aa5e2d13ba1583075542d3f0789ced7ef0664a53
latp1102 <latp1102@gmail.com>
2025-03-02 00:10:45 +0700

    Session18-4

---
+++ b4.js
@@ -0,0 +1,37 @@
+let number1 = prompt("Nhập vào số");
+
+switch (number1) {
+    case "0":
+        document.write('số không');
+        break;
+    case "1":
+        document.write('số một');
+        break;
+    case "2":
+        document.write('số hai');
+        break;
+    case "3":
+        document.write('số ba');
+        break;
+    case "4":
+        document.write('số bốn');
+        break;
+    case "5":
+        document.write('số năm');
+        break;
+    case "6":
+        document.write('số sáu');
+        break;
+    case "7":
+        document.write('số bảy');
+        break;
+    case "8":
+        document.write('số tám');
+        break;
+    case "9":
+        document.write('số chín');
+        break;
+    default:
+        document.write('không tồn tại');
+        break;
+}
